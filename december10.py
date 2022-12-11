af5bb11337cddf33ac353ca163452c3f66a250fa
Martha <martha.brennich@googlemail.com>
2022-12-11 18:56:34 +0100

    december 10, task 1

---
+++ december10.py
@@ -0,0 +1,53 @@
+"""Functions for the December 10 task."""
+
+from typing import Tuple
+from helpers import split_input
+
+OP_TIMES = {"noop": 1, "addx": 2}
+
+
+class Processor:
+    def __init__(self, start_time: int, initial_X: int):
+        self.clock = start_time
+        self.X = initial_X
+        self.value_trace = [initial_X]
+
+    @property
+    def signal(self):
+        signal_points = [20, 60, 100, 140, 180, 220]
+        return sum(
+            signal_strength(i, self.value_trace[i - 1]) for i in signal_points
+        )
+
+    def single_op(self, operation: str):
+        new_clock, new_X = single_op(self.clock, self.X, operation)
+        for _ in range(self.clock + 1, new_clock):
+            self.value_trace.append(self.X)
+        self.value_trace.append(new_X)
+        self.X = new_X
+        self.clock = new_clock
+
+    def op_list(self, instructions):
+        for instruction in split_input(instructions, "\n"):
+            self.single_op(instruction)
+        return self
+
+
+def single_op(
+    initial_clock: int, initial_X: int, operation: str
+) -> Tuple[int, int]:
+    if operation == "noop":
+        return initial_clock + OP_TIMES[operation], initial_X
+    command, value = operation.split(" ")
+    if command == "addx":
+        return initial_clock + OP_TIMES[command], initial_X + int(value)
+
+
+def signal_strength(clock, X):
+    return clock * X
+
+
+if __name__ == "__main__":
+    with open("december10_input.txt") as file:
+        december10_instructions = file.read()
+    print("Signal", Processor(0, 1).op_list(december10_instructions).signal)
---
+++ december10_test.py
@@ -0,0 +1,225 @@
+"""Test for the functions for the December 9 task."""
+
+
+import pytest
+
+from december10 import Processor, single_op, signal_strength
+
+
+INSTRUCTIONS = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+
+@pytest.mark.parametrize(
+    "initial_clock, initial_X, operation, final_clock, final_X",
+    [(0, 1, "noop", 1, 1), (1, 1, "addx 3", 3, 4), (3, 4, "addx -5", 5, -1)],
+)
+def test_single_op(initial_clock, initial_X, operation, final_clock, final_X):
+    clock, X = single_op(initial_clock, initial_X, operation)
+    assert clock == final_clock
+    assert X == final_X
+
+
+@pytest.mark.parametrize(
+    "clock, X, signal",
+    [
+        (0, 1, 0),
+        (1, 1, 1),
+        (20, 21, 420),
+        (60, 19, 1140),
+        (100, 18, 1800),
+        (140, 21, 2940),
+    ],
+)
+def test_signal_strength(clock, X, signal):
+    assert signal_strength(clock, X) == signal
+
+
+def test_nth_X_manual_ops():
+    proc = Processor(0, 1)
+    proc.single_op("noop")
+    assert proc.value_trace[1] == 1
+    assert proc.value_trace == [1, 1]
+    proc.single_op("addx 3")
+    assert proc.value_trace[1] == 1
+    assert proc.value_trace[2] == 1
+    assert proc.value_trace[3] == 4
+    assert proc.value_trace == [1, 1, 1, 4]
+    proc.single_op("addx -5")
+    assert proc.value_trace[1] == 1
+    assert proc.value_trace[2] == 1
+    assert proc.value_trace[3] == 4
+    assert proc.value_trace[4] == 4
+    assert proc.value_trace[5] == -1
+    assert proc.value_trace == [1, 1, 1, 4, 4, -1]
+    proc.single_op("noop")
+    assert proc.value_trace == [1, 1, 1, 4, 4, -1, -1]
+    proc.single_op("noop")
+    assert proc.value_trace == [1, 1, 1, 4, 4, -1, -1, -1]
+    proc.single_op("noop")
+    proc.single_op("addx 0")
+    assert proc.value_trace == [1, 1, 1, 4, 4, -1, -1, -1, -1, -1, -1]
+
+
+def test_nth_X_op_list():
+    proc = Processor(0, 1)
+    proc.op_list(INSTRUCTIONS)
+    print(proc.clock)
+    assert proc.value_trace[20 - 1] == 21
+    assert proc.value_trace[60 - 1] == 19
+    assert proc.value_trace[100 - 1] == 18
+    assert proc.value_trace[140 - 1] == 21
+    assert proc.value_trace[180 - 1] == 16
+    assert proc.value_trace[220 - 1] == 18
+
+
+def test_total_signal():
+    proc = Processor(0, 1)
+
+    assert proc.op_list(INSTRUCTIONS).signal == 13140
